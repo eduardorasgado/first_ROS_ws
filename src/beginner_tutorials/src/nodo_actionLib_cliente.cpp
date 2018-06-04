8c60ead310d76b63328723a14dbbd408d7a9abba
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-04 05:26:00 -0500

    client node cpp changed in bt

--- src/beginner_tutorials/src/nodo_actionLib_cliente.cpp
+++ src/beginner_tutorials/src/nodo_actionLib_cliente.cpp
@@ -2,6 +2,10 @@
 /**Debemos incluir el Action al final del nombre del action
 con ello le decimos que se trata de una accion
 **/
-#include "beginner_tutorials/Test.h"
+#include <beginner_tutorials/TestAction.h>
+#include <actionlib/client/simple_action_client>
+
+typedef actionlib::SimpleActionClinet<beginner_tutorials::TestAction> Client;
+
 
 
