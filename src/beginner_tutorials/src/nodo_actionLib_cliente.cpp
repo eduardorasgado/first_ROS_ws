9f0db1fdd2373ee5ea3b6ab472ccae3e6cb1b20d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-04 05:02:18 -0500

    nodo_ActionLib_servido in beginner_tutorials wrote

--- src/beginner_tutorials/src/nodo_actionLib_cliente.cpp
+++ src/beginner_tutorials/src/nodo_actionLib_cliente.cpp
@@ -1,3 +1,7 @@
 #include "ros/ros.h"
+/**Debemos incluir el Action al final del nombre del action
+con ello le decimos que se trata de una accion
+**/
 #include "beginner_tutorials/Test.h"
+
 
--- src/beginner_tutorials/src/nodo_actionLib_servidor.cpp
+++ src/beginner_tutorials/src/nodo_actionLib_servidor.cpp
@@ -1,3 +1,31 @@
 #include "ros/ros.h"
-#include "beginner_tutorials/Test.h"
+#include <beginner_tutorials/TestAction.h>
+#include <actionlib/server/simple_Action_server.h>
 
+//para simplificar la biblioteca
+typedef actionlib::SimpleActionServer<beginner_tutorials::TestAction>Server;
+
+/**La funcion del actionlib server**/
+void actionLib_callback(const beginner_tutorials::TestGoalConstPtr& goal, Server* as){
+	as->setSucceeded();
+	ROS_INFO("ActionLib goal recibido");
+
+	//emitimos feedback
+	beginner_tutorials::TestFeedback feedback;
+	feedback.infoFeedback = 0;
+
+	as->publishFeedback(feedback);
+	ROS_INFO("ActionLib feedback emitido");
+}
+
+/**Funcion principal**/
+int main (int argc, chat **argv){
+	//registramos el nombre del nodo
+	ros::init(argc, argv, "nodo_actionLib_servidor");
+	ros::NodeHandle n;
+
+	//registramos el servicio
+	Server server(n, "testALService", boost::bind(&actionLib_callback, _1, &server), false);
+	server.start();
+	ROS_INFO("ActionLib servicio registrado");
+}
