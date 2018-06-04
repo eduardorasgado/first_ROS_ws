a5c80af7e3c382e5b7d95c6b05d36e7823522089
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-04 05:08:03 -0500

    code to actionlib server in beginner_tutorials

--- src/beginner_tutorials/src/nodo_actionLib_servidor.cpp
+++ src/beginner_tutorials/src/nodo_actionLib_servidor.cpp
@@ -2,7 +2,8 @@
 #include <beginner_tutorials/TestAction.h>
 #include <actionlib/server/simple_Action_server.h>
 
-//para simplificar la biblioteca
+//para simplificar la biblioteca, en lugar
+//de escribir todo esto, simplemente escribimos Server
 typedef actionlib::SimpleActionServer<beginner_tutorials::TestAction>Server;
 
 /**La funcion del actionlib server**/
@@ -25,7 +26,12 @@ int main (int argc, chat **argv){
 	ros::NodeHandle n;
 
 	//registramos el servicio
+	//asociamos "testALService" a la funcion manejadora
 	Server server(n, "testALService", boost::bind(&actionLib_callback, _1, &server), false);
 	server.start();
 	ROS_INFO("ActionLib servicio registrado");
+
+	//nos quedamos a la espera de llamadas al servicio
+	ros::spin();
+	return 0;
 }
