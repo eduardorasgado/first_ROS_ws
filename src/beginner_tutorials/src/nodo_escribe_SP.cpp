9f26594271ef8573463e9a1269ee5fa10ae9cfaf
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-05 04:14:36 -0500

    escribe and leer nodes to parameter comunication

---
+++ src/beginner_tutorials/src/nodo_escribe_SP.cpp
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+#include <cstdlib>
+
+int main(int argc, char **argv){
+	//registrar el nodo
+	ros::init(argc, argv, "nodo_Escribe_SP");
+	ros::NodeHandle n;
+
+	//el parametro a enviar se llamara parametroTest
+	//y tendra el valor string "calorPrueba"
+	n.setParam("parametroTest", "valorPrueba");
+
+	//para que no finalice el proceso
+	ros::waitForShutdown();
+	return0;
+}
