bbaac399ea16d76fea53bf6ff6e44fd64ef193e1
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-06 16:49:23 -0500

    timer node code added

--- src/beginner_tutorials/src/nodo_timer_ejemplo.cpp
+++ src/beginner_tutorials/src/nodo_timer_ejemplo.cpp
@@ -6,11 +6,14 @@ void funcionManejadoraTimer(){
 
 int main(int argc, char **argv){
 	//registrar nodo
+	ros::init(argc, argv "nodo_timer");
 	//realizar suscripciones
-
+	ros::NodeHandle n;
+	//suscripciones...
+	Subscriber subscriptor1 = n.subscribe("topic_name", 0, funcionCallbackllamada);
 	//creamos timer
 	ros::Timer timer;
-	timer = nodo.createTimer(ros::Duration(10), funcionManejadoraTimer);
+	timer = n.createTimer(ros::Duration(10), funcionManejadoraTimer);
 
 	ros::AsyncSpinner spinner(n)
 	spinner.start();
